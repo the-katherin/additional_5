9216684ac4bae43a49d3a545764dd2a4cd6d22be
the-katherin <ekaterina082612@gmail.com>
2018-02-24 18:06:37 +0300

    not 100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+  var res = true;
+  var numberOfFirst = 0;
+  var numberOfSecond = 0;
+  if (str.length%2 === 1) {
+     res = false;
+  }
+  else if (str.length%2 === 0) {
+     for (var i =0; i<str.length; i++) {
+       for (var j=0; j<bracketsConfig.length ; j++) {
+         if   (str.charAt(i) === bracketsConfig[j][0]) {
+           numberOfFirst ++ ;
+         }
+       }
+     }
+     for (var x =0; x<str.length; x++) {
+       for (var y=0; y<bracketsConfig.length; y++) {
+         if   (str.charAt(x) === bracketsConfig[y][0]) {
+          numberOfSecond ++ ;
+         }
+       }
+     }
+     if (numberOfFirst !== numberOfSecond)  {
+       res = false;
+     }
+
+  }
+
+  return res;
+
 }
